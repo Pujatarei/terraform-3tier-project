470ecd58ea660f821eb3252a35ca905e34ddba9d
Pujatarei <pujatarei91@gmail.com>
2025-08-01 23:35:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -200,7 +200,10 @@ resource "aws_db_instance" "default" {
 
 resource "aws_db_subnet_group" "default" {
   name       = "main"
-  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+  subnet_ids = [
+      aws_subnet.database-subnet-1.id, 
+      aws_subnet.database-subnet-2.id
+   ]
 
   tags = {
     Name = "My DB subnet group"
@@ -346,7 +349,17 @@ resource "aws_lb_listener" "external-elb" {
     target_group_arn = aws_lb_target_group.external-elb.arn
   }
 }
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+}
 
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "ap-south-1b"  
+}
 
 
 
